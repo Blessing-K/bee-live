2bf0380373112ad342b574b599342825a252890d
Blessing-K <boluwatitobee@gmail.com>
2025-04-30 13:25:59 -0400

    Attept to fix dashboard flash before login page by creating global auth in App.js

---
+++ components/LoadingSpinner.js
@@ -0,0 +1,25 @@
+export default function LoadingSpinner() {
+    return (
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        backgroundColor: '#4B164C' 
+      }}>
+        <div style={{
+          width: '50px',
+          height: '50px',
+          border: '5px solid #00ffff',
+          borderTopColor: 'transparent',
+          borderRadius: '50%',
+          animation: 'spin 1s linear infinite'
+        }}></div>
+        <style jsx>{`
+          @keyframes spin {
+            to { transform: rotate(360deg); }
+          }
+        `}</style>
+      </div>
+    );
+  }
