3274f0b9b383aaeebab2c27cb2824ef602afa18e
Blessing-K <boluwatitobee@gmail.com>
2026-02-02 18:17:24 -0500

    Improve responsive design for mobile view

--- components/CourseForm.js
+++ components/CourseForm.js
@@ -33,9 +33,7 @@ export default function CourseForm({ userId }) {
       <button type="submit" className="btn-primary course-submit">
         + Add Course
       </button>
-      {loading && (
-        <p className="course-loading">Generating smart advice...</p>
-      )}
+      {loading && <p className="course-loading">Generating smart advice...</p>}
     </form>
   );
 }
